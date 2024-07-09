2d3bdbddfb1c45d0739e1469d42e0b37bd890732
Zeljko Bekcic <zeljko.bekcic@iits-consulting.de>
2024-07-09 22:26:27 +0200

    add: better build system

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env zsh
+
+ZMK_APP_PATH="$HOME/Projects/Private/zmk/app"
+ZMK_CONFIG_PATH="$HOME/Projects/Private/temper-zmk-config/config"
+ZMK_EXTRA_MODULES_PATH="$HOME/Projects/Private/temper-zmk-config"
+OUTPUT_DIRECTORY="$HOME/Projects/Private/temper-zmk-config/outputs/"
+
+rm -rf "$OUTPUT_DIRECTORY"
+mkdir -p "$OUTPUT_DIRECTORY"
+
+cd "$ZMK_APP_PATH"
+source ../.venv/bin/activate
+
+yq '.include | map(.board + " " + .shield)' "$ZMK_EXTRA_MODULES_PATH/build.yaml" | while read line; do
+	board=$(echo $line | awk '{ print $2 }')
+	shield=$(echo $line | awk '{ print $3 }')
+
+	west build -d "build/$shield-$board" -b $board --pristine -- -DZMK_CONFIG="$ZMK_CONFIG_PATH" -DSHIELD=$shield -DZMK_EXTRA_MODULES="$ZMK_EXTRA_MODULES_PATH"
+	cp "build/$shield-$board/zephyr/zmk.uf2" "$OUTPUT_DIRECTORY/$shield-$board.uf2"
+done
+
+deactivate
+
+rm -rf build
+
+cd -
