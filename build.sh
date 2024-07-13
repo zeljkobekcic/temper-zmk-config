cbd7b8a041f39ffdc9049560fdeb6a8275ac1839
Zeljko Bekcic <zeljko.bekcic@iits-consulting.de>
2024-07-13 15:17:07 +0200

    chore: rename dongle mode

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env zsh
 
 ZMK_APP_PATH="$HOME/Projects/Private/zmk/app"
-ZMK_CONFIG_PATH="$HOME/Projects/Private/temper-zmk-config/config"
-ZMK_EXTRA_MODULES_PATH="$HOME/Projects/Private/temper-zmk-config"
+ZMK_CONFIG_ROOT_PATH="$HOME/Projects/Private/temper-zmk-config"
 OUTPUT_DIRECTORY="$HOME/Projects/Private/temper-zmk-config/outputs/"
 
 rm -rf "$OUTPUT_DIRECTORY"
@@ -11,11 +10,11 @@ mkdir -p "$OUTPUT_DIRECTORY"
 cd "$ZMK_APP_PATH"
 source ../.venv/bin/activate
 
-yq '.include | map(.board + " " + .shield)' "$ZMK_EXTRA_MODULES_PATH/build.yaml" | while read line; do
+yq '.include | map(.board + " " + .shield)' "$ZMK_CONFIG_ROOT_PATH/build.yaml" | while read line; do
 	board=$(echo $line | awk '{ print $2 }')
 	shield=$(echo $line | awk '{ print $3 }')
 
-	west build -d "build/$shield-$board" -b $board --pristine -- -DZMK_CONFIG="$ZMK_CONFIG_PATH" -DSHIELD=$shield -DZMK_EXTRA_MODULES="$ZMK_EXTRA_MODULES_PATH"
+	west build -d "build/$shield-$board" -b $board --pristine -- -DZMK_CONFIG="$ZMK_CONFIG_ROOT_PATH/config" -DSHIELD=$shield 
 	cp "build/$shield-$board/zephyr/zmk.uf2" "$OUTPUT_DIRECTORY/$shield-$board.uf2"
 done
 
