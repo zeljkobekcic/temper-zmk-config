b44eab60f98e373111f25b8088b512c6329f9948
Zeljko Bekcic <zeljko.bekcic@posteo.net>
2024-10-13 16:29:44 +0200

    make config better

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-ZMK_APP_PATH="$HOME/Projects/Private/zmk/app"
-ZMK_CONFIG_ROOT_PATH="$HOME/Projects/Private/temper-zmk-config"
+ZMK_APP_PATH="$HOME/Projects/private/zmk/app"
+ZMK_CONFIG_ROOT_PATH="$HOME/Projects/private/temper-zmk-config"
 OUTPUT_DIRECTORY="$ZMK_CONFIG_ROOT_PATH/outputs/"
 
 rm -rf "$OUTPUT_DIRECTORY"
@@ -7,6 +7,7 @@ mkdir -p "$OUTPUT_DIRECTORY"
 
 (
 	cd "$ZMK_APP_PATH"
+	echo ">>> $ZMK_APP_PATH"
 	source ../.venv/bin/activate
 
 	# format to json lines then read ech json object per loop iterationn
@@ -14,8 +15,10 @@ mkdir -p "$OUTPUT_DIRECTORY"
 		board=$(echo "$line" | yq -p=json '.board')
 		shield=$(echo "$line" | yq -p=json '.shield')
 
-		west build -d "build/$shield-$board" -b "$board" --pristine -- -DZMK_CONFIG="$ZMK_CONFIG_ROOT_PATH/config" -DSHIELD="$shield" > "$OUTPUT_DIRECTORY/$shield-$board.log"
+		echo ">>> " west build -d "build/$shield-$board" -b "$board" --pristine -- -DZMK_CONFIG="$ZMK_CONFIG_ROOT_PATH/config" -DSHIELD="$shield" '>' "$OUTPUT_DIRECTORY/$shield-$board.log"
+		west build -d "build/$shield-$board" -b "$board" --pristine -- -DZMK_CONFIG="$ZMK_CONFIG_ROOT_PATH/config" -DSHIELD="$shield"  #> "$OUTPUT_DIRECTORY/$shield-$board.log"
 
+		echo ">>>" cp "build/$shield-$board/zephyr/zmk.uf2" "$OUTPUT_DIRECTORY/$shield-$board.uf2"
 		cp "build/$shield-$board/zephyr/zmk.uf2" "$OUTPUT_DIRECTORY/$shield-$board.uf2"
 	done
 
